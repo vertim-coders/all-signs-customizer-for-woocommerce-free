69be09f8f939441485efbde8aa94a5c722a48ff9
Audreyguedegbe <guedegbeaudrey498@gmail.com>
2024-01-26 13:21:52 +0100

    Mise en place de la vue

--- src/frontend/pages/configurateur/index.vue
+++ src/frontend/pages/configurateur/index.vue
@@ -1,10 +1,347 @@
 <template>
-    <div>
-        Interface du configurateur
+    <div class="aso-w-full aso-h-full">
+        <div class="nav-home aso-flex aso-justify-between aso-bg-white aso-h-[60px] aso-rounded-tr-none aso-rounded-bl-none aso-rounded-br-none aso-rounded-tl-md">
+            <div class="aso-pt-6 aso-pl-16">
+                <p class="aso-text-black aso-text-2xl aso-font-bold">Plastic Signs</p>
+            </div>
+            <div class="aso-flex aso-pt-2">
+                <button class="aso-bg-[#22272D] aso-w-32 aso-h-11 aso-p-2 aso-rounded-full">
+                    <div class="aso-flex">
+                        <div class="aso-pt-1">
+                            <svg width="25" height="25" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="simple-line-icons:reload">
+                                <path id="Vector" d="M14.9789 0C7.48448 0 1.38745 6.09744 1.38745 13.5918C1.38745 20.6362 6.81604 26.5274 13.7967 27.1298L11.1056 28.4938C10.7732 28.6762 10.5754 29.0863 10.7723 29.4102L10.8675 29.6563C11.0653 29.9802 11.4937 30.0951 11.8251 29.9136L16.3166 27.6139C16.3223 27.6102 16.3284 27.6112 16.334 27.6074L16.6354 27.4424C16.8018 27.351 16.912 27.2015 16.956 27.0351C17.002 26.8687 16.9809 26.6835 16.882 26.5223L16.7034 26.2293C16.7006 26.2237 16.695 26.2199 16.6917 26.2148L13.9176 21.8798C13.7207 21.5559 13.2932 21.441 12.96 21.6224L12.742 21.7743C12.4096 21.9557 12.3876 22.4245 12.5845 22.7475L14.2214 25.2867C14.2125 25.2862 14.2045 25.2829 14.196 25.2825C8.06477 24.8803 3.26242 19.7442 3.26242 13.5919C3.26245 7.13109 8.51852 1.875 14.9789 1.875C21.4401 1.875 26.7257 7.13109 26.7257 13.5919C26.7257 17.3119 25.0171 20.7286 22.0382 22.966C21.6244 23.2772 21.5409 23.8651 21.8517 24.279C22.163 24.6938 22.7508 24.7763 23.1647 24.4655C26.6199 21.8701 28.6012 17.9068 28.6012 13.5919C28.6007 6.09753 22.4737 0 14.9789 0Z" fill="white"/>
+                                </g>
+                            </svg>
+                        </div>
+                        
+                        <div class="aso-pl-4 aso-pt-3.5">
+                            <p class="aso-text-white aso-w-20 aso-h-5">Restart all </p>
+                        </div>
+                    </div>
+                </button>
+                    
+                <div class="aso-flex">
+                    <div class="aso-flex">
+
+                        <div class="aso-relative -aso-left-14">
+
+                            <button class="aso-absolute cercle aso-rounded-full aso-bg-white">
+
+                                <svg class="aso-absolute aso-top-3 aso-right-4" width="22" height="22" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g id="majesticons:undo-line">
+                                    <path id="Vector" d="M5 11.625V18.0833H11.25M25 20.6666C24.3787 14.8541 20.7912 10.3333 15 10.3333C11.5875 10.3333 7.58875 13.2628 5.8825 17.4375" stroke="#3D3D3D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </g>
+                                </svg>
+                            
+                            </button>
+
+                        </div>
+
+                        <div class="aso-pl-24 aso-pt-4">
+                            <p class="text-sm">Undo</p>
+                        </div>
+
+                    </div>
+                    <div class="aso-flex">
+
+                        <div class="aso-relative -aso-left-14">
+
+                            <button class="aso-absolute cercle aso-rounded-full aso-bg-white">
+
+                                
+                                <svg class="aso-absolute aso-top-3 aso-right-4" width="22" height="22" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g id="majesticons:redo">
+                                    <path id="Vector" d="M25 11.625V18.0833H18.75M5 20.6666C5.62125 14.8541 9.20875 10.3333 15 10.3333C18.4125 10.3333 22.4113 13.2628 24.1175 17.4375" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </g>
+                                </svg>
+
+                            </button>
+
+                        </div>
+
+                        <div class="aso-pl-24 aso-pt-4">
+                            <p class="text-sm">Redo</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="aso-pt-4">
+  
+                <p class="aso-text-black aso-text-3xl aso-font-bold">$ 495,09</p>
+
+            </div>
+
+            
+
+            <button class="aso-w-28 aso-bg-[#FFBC3C] aso-h-16 aso-rounded-tr-md aso-rounded-bl-none aso-rounded-br-none aso-rounded-tl-none aso-p-6">
+                <p class="aso-text-white aso-text-lg aso-font-bold">Terminer</p>
+            </button>
+
+            
+        </div>
+        
+        <div class="aso-flex aso-h-[583px]">
+            
+            <div class="aso-bg-[#E7ECEF] aso-flex aso-flex-col aso-items-center aso-justify-between aso-w-[98%]">
+
+                <div class="aso-relative aso-w-[55%] aso-h-[55%] aso-top-8">
+  
+                    <div class="aso-flex aso-justify-end aso-item-end aso-bg-red-100">
+
+                        <div class="aso-absolute aso-h-full aso-w-0.5 aso-bg-red-400">
+
+                            <div class="aso-absolute aso-w-4 aso-h-0.5 aso-bg-black"></div>
+                            <div class="aso-absolute aso-bottom-0 aso-w-4 aso-h-0.5 aso-bg-black"></div>
+                            <div class="aso-absolute aso-top-24 aso-origin-center aso-rotate-[90deg]">
+                                <p class="">65mm</p>
+                            </div>
+                        </div>
+
+                        <div class="aso-w-full aso-h-full">
+                            <img class="aso-w-full aso-h-full" :src="image" alt="">
+                        </div>
+
+                        <div class="aso-absolute aso-w-full aso-h-0.5 aso-bottom-0 aso-bg-black">
+
+                            <div class="aso-absolute aso-left-0 aso-h-4 aso-w-0.5 aso-bg-black"></div>
+                            <div class="aso-absolute aso-right-0 aso-h-4 aso-w-0.5 aso-bg-black"></div>
+                            <div class="aso-absolute aso-w-full aso-h-full aso-justify-center">
+                                <p class="aso-text-center">155mm</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="aso-bg-white aso-w-[10%] aso-pt-12 aso-flex aso-flex-col justify-between">
+              
+
+                <div class="aso-flex">
+
+                    <div class="aso-relative -aso-left-12">
+
+                        <button class="aso-absolute cercle aso-rounded-full aso-bg-white">
+
+                            <svg class="aso-absolute aso-top-3 aso-right-4" width="22" height="22" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="majesticons:undo-line">
+                                <path id="Vector" d="M5 11.625V18.0833H11.25M25 20.6666C24.3787 14.8541 20.7912 10.3333 15 10.3333C11.5875 10.3333 7.58875 13.2628 5.8825 17.4375" stroke="#3D3D3D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </g>
+                            </svg>
+                        
+                        </button>
+
+                    </div>
+
+                    <div class="aso-pl-[42px] aso-pt-[60px]">
+                        <p class="text-sm">Undo</p>
+                    </div>
+
+                </div>
+
+                
+
+                <div class="aso-flex">
+
+                    <div class="aso-relative -aso-left-12">
+
+                        <button class="aso-absolute cercle aso-rounded-full aso-bg-white">
+
+                            <svg class="aso-absolute aso-top-3 aso-right-4" width="22" height="22" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="majesticons:undo-line">
+                                <path id="Vector" d="M5 11.625V18.0833H11.25M25 20.6666C24.3787 14.8541 20.7912 10.3333 15 10.3333C11.5875 10.3333 7.58875 13.2628 5.8825 17.4375" stroke="#3D3D3D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </g>
+                            </svg>
+                        
+                        </button>
+
+                    </div>
+
+                    <div class="aso-pl-[42px] aso-pt-[60px]">
+                        <p class="text-sm">Undo</p>
+                    </div>
+
+                </div>
+
+                <div class="aso-flex">
+
+                    <div class="aso-relative -aso-left-12">
+
+                        <button class="aso-absolute cercle aso-rounded-full aso-bg-white">
+
+                            <svg class="aso-absolute aso-top-3 aso-right-4" width="22" height="22" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="majesticons:undo-line">
+                                <path id="Vector" d="M5 11.625V18.0833H11.25M25 20.6666C24.3787 14.8541 20.7912 10.3333 15 10.3333C11.5875 10.3333 7.58875 13.2628 5.8825 17.4375" stroke="#3D3D3D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </g>
+                            </svg>
+                        
+                        </button>
+
+                    </div>
+
+                    <div class="aso-pl-[42px] aso-pt-[60px]">
+                        <p class="text-sm">Undo</p>
+                    </div>
+
+                </div>
+
+                <div class="aso-flex">
+
+                    <div class="aso-relative -aso-left-12">
+
+                        <button class="aso-absolute cercle aso-rounded-full aso-bg-white">
+
+                            <svg class="aso-absolute aso-top-3 aso-right-4" width="22" height="22" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="majesticons:undo-line">
+                                <path id="Vector" d="M5 11.625V18.0833H11.25M25 20.6666C24.3787 14.8541 20.7912 10.3333 15 10.3333C11.5875 10.3333 7.58875 13.2628 5.8825 17.4375" stroke="#3D3D3D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </g>
+                            </svg>
+                        
+                        </button>
+
+                    </div>
+
+                    <div class="aso-pl-[42px] aso-pt-[60px]">
+                        <p class="text-sm">Undo</p>
+                    </div>
+
+                </div>
+            </div>
+            
+        </div>
+
     </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+
+var image = ref("../../../../assets/images/configurateur/Screenshot 2024-01-09 at 15.26 1.png");
+
+const isColorVisible = ref(false);
+const isFixingVisible = ref(false);
+const isShapeVisible = ref(false);
+const isSizeVisible = ref(false);
+const isMaterialVisible = ref(false);
+const isProductVisible = ref(false);
+
+const color = () => {
+  isColorVisible.value = !isColorVisible.value;
+  if (isFixingVisible.value) {
+    isFixingVisible.value = false;
+  }
+  if (isShapeVisible.value) {
+    isShapeVisible.value = false;
+  }
+  if (isSizeVisible.value) {
+    isSizeVisible.value = false;
+  }
+  if (isMaterialVisible.value) {
+    isMaterialVisible.value = false;
+  }
+  if (isProductVisible.value) {
+    isProductVisible.value = false;
+  }
+};
+
+const fixing = () => {
+  isFixingVisible.value = !isFixingVisible.value;
+  if (isColorVisible.value) {
+    isColorVisible.value = false;
+  }
+  if (isShapeVisible.value) {
+    isShapeVisible.value = false;
+  }
+  if (isSizeVisible.value) {
+    isSizeVisible.value = false;
+  }
+  if (isMaterialVisible.value) {
+    isMaterialVisible.value = false;
+  }
+  if (isProductVisible.value) {
+    isProductVisible.value = false;
+  }
+};
+
+const shape = () => {
+  isShapeVisible.value = !isShapeVisible.value;
+  if (isColorVisible.value) {
+    isColorVisible.value = false;
+  }
+  if (isFixingVisible.value) {
+    isFixingVisible.value = false;
+  }
+  if (isSizeVisible.value) {
+    isSizeVisible.value = false;
+  }
+  if (isMaterialVisible.value) {
+    isMaterialVisible.value = false;
+  }
+  if (isProductVisible.value) {
+    isProductVisible.value = false;
+  }
+};
+
+const size = () => {
+  isSizeVisible.value = !isSizeVisible.value;
+  if (isColorVisible.value) {
+    isColorVisible.value = false;
+  }
+  if (isFixingVisible.value) {
+    isFixingVisible.value = false;
+  }
+  if (isShapeVisible.value) {
+    isShapeVisible.value = false;
+  }
+  if (isMaterialVisible.value) {
+    isMaterialVisible.value = false;
+  }
+  if (isProductVisible.value) {
+    isProductVisible.value = false;
+  }
+};
+
+const material = () => {
+  isMaterialVisible.value = !isMaterialVisible.value;
+  if (isColorVisible.value) {
+    isColorVisible.value = false;
+  }
+  if (isFixingVisible.value) {
+    isFixingVisible.value = false;
+  }
+  if (isSizeVisible.value) {
+    isSizeVisible.value = false;
+  }
+  if (isShapeVisible.value) {
+    isShapeVisible.value = false;
+  }
+  if (isProductVisible.value) {
+    isProductVisible.value = false;
+  }
+};
+
+const product = () => {
+  isProductVisible.value = !isProductVisible.value;
+  if (isColorVisible.value) {
+    isColorVisible.value = false;
+  }
+  if (isFixingVisible.value) {
+    isFixingVisible.value = false;
+  }
+  if (isSizeVisible.value) {
+    isSizeVisible.value = false;
+  }
+  if (isShapeVisible.value) {
+    isShapeVisible.value = false;
+  }
+  if (isMaterialVisible.value) {
+    isMaterialVisible.value = false;
+  }
+};
 
 </script>
