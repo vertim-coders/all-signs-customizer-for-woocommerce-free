1098565f8c10367a646374854b262be9c1f2ffff
Audreyguedegbe <guedegbeaudrey498@gmail.com>
2024-05-16 19:42:36 +0200

    modification sur la page de la list of configuration et de la page list of  material

--- src/admin/pages/configuration/materials/index.vue
+++ src/admin/pages/configuration/materials/index.vue
@@ -1,40 +1,43 @@
 <template>
     <div>
         <div v-if="!isNewComponent">
-            <div class="aso-bg-[#F8F9FB] aso-text-[16px] aso-space-x-1 aso-px-4 aso-py-4 aso-flex">
-                <div  v-if="config.trim() != ''" class="aso-font-bold">
-                    {{config}}
+            <div class="aso-sticky aso-top-[70px] aso-z-[999]">
+                <div class="aso-bg-[#F8F9FB] aso-text-[16px] aso-space-x-1 aso-px-4 aso-py-4 aso-flex">
+                    <div  v-if="config.trim() != ''" class="aso-font-bold">
+                        {{config}}
+                    </div>
+                    <img  v-if="config.trim() != ''" class="aso-w-4 aso-h-4 aso-py-1" src="../../../../../assets/icons/ic_crochet.svg" alt="">
+                    <div v-if="config.trim() != ''" >
+                        Material
+                    </div>
                 </div>
-                <img  v-if="config.trim() != ''" class="aso-w-4 aso-h-4 aso-py-1" src="../../../../../assets/icons/ic_crochet.svg" alt="">
-                <div v-if="config.trim() != ''" >
-                    Material
+                <div v-if="!isFetching" class="aso-flex aso-justify-end aso-items-center aso-space-x-2 aso-bg-[#F8F9FB] aso-px-4 aso-py-4">
+                    
+                    <div class="aso-w-1/4">
+                        <button class="aso-flex aso-w-fit aso-h-[fit] aso-rounded aso-bg-[#016464] aso-px-4 aso-space-x-2 aso-p-1.5 aso-border-none aso-text-white aso-opacity-90 hover:aso-opacity-100 aso-cursor-pointer" @click="addComponent">
+                            <svg class="aso-w-5 aso-h-5" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="plus-lg">
+                                <path id="Vector" fill-rule="evenodd" clip-rule="evenodd" d="M11 2.75C11.1823 2.75 11.3572 2.82243 11.4861 2.95136C11.6151 3.0803 11.6875 3.25516 11.6875 3.4375V10.3125H18.5625C18.7448 10.3125 18.9197 10.3849 19.0486 10.5139C19.1776 10.6428 19.25 10.8177 19.25 11C19.25 11.1823 19.1776 11.3572 19.0486 11.4861C18.9197 11.6151 18.7448 11.6875 18.5625 11.6875H11.6875V18.5625C11.6875 18.7448 11.6151 18.9197 11.4861 19.0486C11.3572 19.1776 11.1823 19.25 11 19.25C10.8177 19.25 10.6428 19.1776 10.5139 19.0486C10.3849 18.9197 10.3125 18.7448 10.3125 18.5625V11.6875H3.4375C3.25516 11.6875 3.0803 11.6151 2.95136 11.4861C2.82243 11.3572 2.75 11.1823 2.75 11C2.75 10.8177 2.82243 10.6428 2.95136 10.5139C3.0803 10.3849 3.25516 10.3125 3.4375 10.3125H10.3125V3.4375C10.3125 3.25516 10.3849 3.0803 10.5139 2.95136C10.6428 2.82243 10.8177 2.75 11 2.75Z" fill="white"/>
+                                </g>
+                            </svg>
+                            <div class="aso-text-[14px]">
+                                Add new Material
+                            </div>
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div v-if="!isFetching" class="aso-flex aso-justify-end aso-items-center aso-space-x-2 aso-bg-[#F8F9FB] aso-px-4 aso-py-4">
-                
-                <div class="aso-w-1/4">
-                    <button class="aso-flex aso-w-fit aso-h-[fit] aso-rounded aso-bg-[#016464] aso-px-4 aso-space-x-2 aso-p-1.5 aso-border-none aso-text-white aso-opacity-90 hover:aso-opacity-100 aso-cursor-pointer" @click="addComponent">
-                        <svg class="aso-w-5 aso-h-5" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g id="plus-lg">
-                            <path id="Vector" fill-rule="evenodd" clip-rule="evenodd" d="M11 2.75C11.1823 2.75 11.3572 2.82243 11.4861 2.95136C11.6151 3.0803 11.6875 3.25516 11.6875 3.4375V10.3125H18.5625C18.7448 10.3125 18.9197 10.3849 19.0486 10.5139C19.1776 10.6428 19.25 10.8177 19.25 11C19.25 11.1823 19.1776 11.3572 19.0486 11.4861C18.9197 11.6151 18.7448 11.6875 18.5625 11.6875H11.6875V18.5625C11.6875 18.7448 11.6151 18.9197 11.4861 19.0486C11.3572 19.1776 11.1823 19.25 11 19.25C10.8177 19.25 10.6428 19.1776 10.5139 19.0486C10.3849 18.9197 10.3125 18.7448 10.3125 18.5625V11.6875H3.4375C3.25516 11.6875 3.0803 11.6151 2.95136 11.4861C2.82243 11.3572 2.75 11.1823 2.75 11C2.75 10.8177 2.82243 10.6428 2.95136 10.5139C3.0803 10.3849 3.25516 10.3125 3.4375 10.3125H10.3125V3.4375C10.3125 3.25516 10.3849 3.0803 10.5139 2.95136C10.6428 2.82243 10.8177 2.75 11 2.75Z" fill="white"/>
-                            </g>
-                        </svg>
-                        <div class="aso-text-[14px]">
-                            Add new Material
-                        </div>
-                    </button>
+                <div class="aso-grid aso-grid-cols-5 aso-justify-center aso-items-center aso-p-4 aso-text-sm aso-font-medium aso-text-gray-900 aso-bg-gray-100 aso-border-t aso-border-b aso-border-gray-200 aso-gap-x-16">
+                    <div class="aso-flex aso-items-center aso-justify-center">Material Name</div>
+                    <div class="aso-flex aso-items-center aso-justify-center">Description</div>
+                    <div class="aso-flex aso-items-center aso-justify-center">Icon</div>
+                    <div class="aso-flex aso-items-center aso-justify-center">Behavior (type)</div>
+                    <div class="aso-flex aso-items-center aso-justify-center">Actions</div>
                 </div>
             </div>
             <!-- Table which display all materials -->
             <div class="aso-w-full aso-overflow-x-auto">
                 <div class="aso-overflow-hidden aso-w-full">
-                    <div class="aso-grid aso-grid-cols-5 aso-justify-center aso-items-center aso-p-4 aso-text-sm aso-font-medium aso-text-gray-900 aso-bg-gray-100 aso-border-t aso-border-b aso-border-gray-200 aso-gap-x-16 dark:aso-bg-gray-800 dark:aso-border-gray-700 dark:aso-text-white">
-                        <div class="aso-flex aso-items-center aso-justify-center">Material Name</div>
-                        <div class="aso-flex aso-items-center aso-justify-center">Description</div>
-                        <div class="aso-flex aso-items-center aso-justify-center">Icon</div>
-                        <div class="aso-flex aso-items-center aso-justify-center">Behavior (type)</div>
-                        <div class="aso-flex aso-items-center aso-justify-center">Actions</div>
-                    </div>
+                    
                     <div v-if="isFetching" class="aso-bg-white aso-border-solid aso-border aso-border-[#D1D1D1] aso-flex aso-flex-col aso-space-y-2 aso-justify-center aso-items-center aso-w-full aso-h-[306px] p-4">
                         <img class="aso-w-[200px] aso-h-[200px]" src="../../../../../assets/icons/ic_loading.svg" alt="">
                     </div>
@@ -43,7 +46,7 @@
                             <p class="aso-text-2xl aso-font-bold">{{notFoundMessage}}</p>
                         </div>
                     </div>
-                    <div v-for="(material,key) in materials" :key="key" class="aso-cursor-pointer aso-grid aso-items-center aso-bg-white aso-grid-cols-5 aso-px-4 aso-py-3 aso-text-sm aso-text-gray-700 aso-border-b aso-border-solid aso-border-gray-200 aso-gap-x-16 dark:aso-border-gray-700">
+                    <div v-for="(material,key) in materials" :key="key" class="aso-cursor-pointer aso-grid aso-items-center aso-bg-white aso-grid-cols-5 aso-px-4 aso-py-3 aso-text-sm aso-text-gray-700 aso-border-b-1 aso-border-t-0 aso-border-l-0 aso-border-r-0 aso-border-solid aso-border-gray-200 aso-gap-x-16">
                         <div class="aso-text-gray-500 dark:aso-text-gray-400 aso-flex aso-items-center aso-overflow-hidden aso-whitespace-nowrap aso-text-ellipsis aso-space-x-4">
                             <span class="aso-w-5 aso-h-5 aso-p-1 aso-px-1 aso-flex aso-justify-center aso-rounded-full aso-bg-[#f0f0f1] aso-border aso-border-solid aso-border-black ">
                                 <span class="aso-text-[12px]">{{getInitials(material.name)}}</span> 
