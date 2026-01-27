c1ea15e4e8d4f7714722c556fe09eb11742d499d
Cédrick OGOUBIYI <cedrickogoubiyi13@gmail.com>
2026-01-27 15:24:17 +0100

    debug du chargement de template sticker en prod

--- assets/js/runtime.js
+++ assets/js/runtime.js
@@ -21,12 +21,6 @@
 /******/ 			if (cachedModule.error !== undefined) throw cachedModule.error;
 /******/ 			return cachedModule.exports;
 /******/ 		}
-/******/ 		// Check if module exists (development only)
-/******/ 		if (__webpack_modules__[moduleId] === undefined) {
-/******/ 			var e = new Error("Cannot find module '" + moduleId + "'");
-/******/ 			e.code = 'MODULE_NOT_FOUND';
-/******/ 			throw e;
-/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
 /******/ 			id: moduleId,
@@ -126,7 +120,7 @@
 /******/ 			__webpack_require__.r(ns);
 /******/ 			var def = {};
 /******/ 			leafPrototypes = leafPrototypes || [null, getProto({}), getProto([]), getProto(getProto)];
-/******/ 			for(var current = mode & 2 && value; (typeof current == 'object' || typeof current == 'function') && !~leafPrototypes.indexOf(current); current = getProto(current)) {
+/******/ 			for(var current = mode & 2 && value; typeof current == 'object' && !~leafPrototypes.indexOf(current); current = getProto(current)) {
 /******/ 				Object.getOwnPropertyNames(current).forEach(function(key) { def[key] = function() { return value[key]; }; });
 /******/ 			}
 /******/ 			def['default'] = function() { return value; };
@@ -166,10 +160,10 @@
 /******/ 	
 /******/ 	/* webpack/runtime/get mini-css chunk filename */
 /******/ 	!function() {
-/******/ 		// This function allow to reference all chunks
+/******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "../css/" + chunkId + ".css";
+/******/ 			return undefined;
 /******/ 		};
 /******/ 	}();
 /******/ 	
@@ -180,7 +174,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b78d1623b6d992ee5b15"; }
+/******/ 		__webpack_require__.h = function() { return "bcb85ccd91c46559037c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -220,6 +214,7 @@
 /******/ 				script = document.createElement('script');
 /******/ 		
 /******/ 				script.charset = 'utf-8';
+/******/ 				script.timeout = 120;
 /******/ 				if (__webpack_require__.nc) {
 /******/ 					script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 				}
@@ -443,7 +438,7 @@
 /******/ 					if (idx >= 0) registeredStatusHandlers.splice(idx, 1);
 /******/ 				},
 /******/ 		
-/******/ 				// inherit from previous dispose call
+/******/ 				//inherit from previous dispose call
 /******/ 				data: currentModuleData[moduleId]
 /******/ 			};
 /******/ 			currentChildModule = undefined;
@@ -527,8 +522,7 @@
 /******/ 									update.m,
 /******/ 									promises,
 /******/ 									currentUpdateApplyHandlers,
-/******/ 									updatedModules,
-/******/ 									update.css
+/******/ 									updatedModules
 /******/ 								);
 /******/ 								return promises;
 /******/ 							}, [])
@@ -536,10 +530,11 @@
 /******/ 							return waitForBlockingPromises(function () {
 /******/ 								if (applyOnUpdate) {
 /******/ 									return internalApply(applyOnUpdate);
+/******/ 								} else {
+/******/ 									return setStatus("ready").then(function () {
+/******/ 										return updatedModules;
+/******/ 									});
 /******/ 								}
-/******/ 								return setStatus("ready").then(function () {
-/******/ 									return updatedModules;
-/******/ 								});
 /******/ 							});
 /******/ 						});
 /******/ 					});
@@ -597,50 +592,38 @@
 /******/ 			};
 /******/ 		
 /******/ 			var outdatedModules = [];
-/******/ 		
-/******/ 			var onAccepted = function () {
-/******/ 				return Promise.all([disposePromise, applyPromise]).then(function () {
-/******/ 					// handle errors in accept handlers and self accepted module load
-/******/ 					if (error) {
-/******/ 						return setStatus("fail").then(function () {
-/******/ 							throw error;
-/******/ 						});
-/******/ 					}
-/******/ 		
-/******/ 					if (queuedInvalidatedModules) {
-/******/ 						return internalApply(options).then(function (list) {
-/******/ 							outdatedModules.forEach(function (moduleId) {
-/******/ 								if (list.indexOf(moduleId) < 0) list.push(moduleId);
-/******/ 							});
-/******/ 							return list;
-/******/ 						});
-/******/ 					}
-/******/ 		
-/******/ 					return setStatus("idle").then(function () {
-/******/ 						return outdatedModules;
-/******/ 					});
-/******/ 				});
-/******/ 			};
-/******/ 		
-/******/ 			return Promise.all(
-/******/ 				results
-/******/ 					.filter(function (result) {
-/******/ 						return result.apply;
-/******/ 					})
-/******/ 					.map(function (result) {
-/******/ 						return result.apply(reportError);
-/******/ 					})
-/******/ 			)
-/******/ 				.then(function (applyResults) {
-/******/ 					applyResults.forEach(function (modules) {
-/******/ 						if (modules) {
-/******/ 							for (var i = 0; i < modules.length; i++) {
-/******/ 								outdatedModules.push(modules[i]);
-/******/ 							}
+/******/ 			results.forEach(function (result) {
+/******/ 				if (result.apply) {
+/******/ 					var modules = result.apply(reportError);
+/******/ 					if (modules) {
+/******/ 						for (var i = 0; i < modules.length; i++) {
+/******/ 							outdatedModules.push(modules[i]);
 /******/ 						}
+/******/ 					}
+/******/ 				}
+/******/ 			});
+/******/ 		
+/******/ 			return Promise.all([disposePromise, applyPromise]).then(function () {
+/******/ 				// handle errors in accept handlers and self accepted module load
+/******/ 				if (error) {
+/******/ 					return setStatus("fail").then(function () {
+/******/ 						throw error;
 /******/ 					});
-/******/ 				})
-/******/ 				.then(onAccepted);
+/******/ 				}
+/******/ 		
+/******/ 				if (queuedInvalidatedModules) {
+/******/ 					return internalApply(options).then(function (list) {
+/******/ 						outdatedModules.forEach(function (moduleId) {
+/******/ 							if (list.indexOf(moduleId) < 0) list.push(moduleId);
+/******/ 						});
+/******/ 						return list;
+/******/ 					});
+/******/ 				}
+/******/ 		
+/******/ 				return setStatus("idle").then(function () {
+/******/ 					return outdatedModules;
+/******/ 				});
+/******/ 			});
 /******/ 		}
 /******/ 		
 /******/ 		function applyInvalidatedModules() {
@@ -666,7 +649,7 @@
 /******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
 /******/ 		var document = __webpack_require__.g.document;
 /******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript && document.currentScript.tagName.toUpperCase() === 'SCRIPT')
+/******/ 			if (document.currentScript)
 /******/ 				scriptUrl = document.currentScript.src;
 /******/ 			if (!scriptUrl) {
 /******/ 				var scripts = document.getElementsByTagName("script");
@@ -679,7 +662,7 @@
 /******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
 /******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
 /******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-/******/ 		scriptUrl = scriptUrl.replace(/^blob:/, "").replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
 /******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	}();
 /******/ 	
@@ -786,7 +769,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
 /******/ 	!function() {
-/******/ 		__webpack_require__.b = (typeof document !== 'undefined' && document.baseURI) || self.location.href;
+/******/ 		__webpack_require__.b = document.baseURI || self.location.href;
 /******/ 		
 /******/ 		// object to store loaded and loading chunks
 /******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
@@ -940,12 +923,16 @@
 /******/ 			for (var moduleId in currentUpdate) {
 /******/ 				if (__webpack_require__.o(currentUpdate, moduleId)) {
 /******/ 					var newModuleFactory = currentUpdate[moduleId];
-/******/ 					var result = newModuleFactory
-/******/ 						? getAffectedModuleEffects(moduleId)
-/******/ 						: {
-/******/ 								type: "disposed",
-/******/ 								moduleId: moduleId
-/******/ 							};
+/******/ 					/** @type {TODO} */
+/******/ 					var result;
+/******/ 					if (newModuleFactory) {
+/******/ 						result = getAffectedModuleEffects(moduleId);
+/******/ 					} else {
+/******/ 						result = {
+/******/ 							type: "disposed",
+/******/ 							moduleId: moduleId
+/******/ 						};
+/******/ 					}
 /******/ 					/** @type {Error|false} */
 /******/ 					var abortError = false;
 /******/ 					var doApply = false;
@@ -1104,7 +1091,6 @@
 /******/ 					}
 /******/ 				},
 /******/ 				apply: function (reportError) {
-/******/ 					var acceptPromises = [];
 /******/ 					// insert new code
 /******/ 					for (var updateModuleId in appliedUpdate) {
 /******/ 						if (__webpack_require__.o(appliedUpdate, updateModuleId)) {
@@ -1141,9 +1127,8 @@
 /******/ 									}
 /******/ 								}
 /******/ 								for (var k = 0; k < callbacks.length; k++) {
-/******/ 									var result;
 /******/ 									try {
-/******/ 										result = callbacks[k].call(null, moduleOutdatedDependencies);
+/******/ 										callbacks[k].call(null, moduleOutdatedDependencies);
 /******/ 									} catch (err) {
 /******/ 										if (typeof errorHandlers[k] === "function") {
 /******/ 											try {
@@ -1180,63 +1165,54 @@
 /******/ 											}
 /******/ 										}
 /******/ 									}
-/******/ 									if (result && typeof result.then === "function") {
-/******/ 										acceptPromises.push(result);
-/******/ 									}
 /******/ 								}
 /******/ 							}
 /******/ 						}
 /******/ 					}
 /******/ 		
-/******/ 					var onAccepted = function () {
-/******/ 						// Load self accepted modules
-/******/ 						for (var o = 0; o < outdatedSelfAcceptedModules.length; o++) {
-/******/ 							var item = outdatedSelfAcceptedModules[o];
-/******/ 							var moduleId = item.module;
-/******/ 							try {
-/******/ 								item.require(moduleId);
-/******/ 							} catch (err) {
-/******/ 								if (typeof item.errorHandler === "function") {
-/******/ 									try {
-/******/ 										item.errorHandler(err, {
-/******/ 											moduleId: moduleId,
-/******/ 											module: __webpack_require__.c[moduleId]
-/******/ 										});
-/******/ 									} catch (err1) {
-/******/ 										if (options.onErrored) {
-/******/ 											options.onErrored({
-/******/ 												type: "self-accept-error-handler-errored",
-/******/ 												moduleId: moduleId,
-/******/ 												error: err1,
-/******/ 												originalError: err
-/******/ 											});
-/******/ 										}
-/******/ 										if (!options.ignoreErrored) {
-/******/ 											reportError(err1);
-/******/ 											reportError(err);
-/******/ 										}
-/******/ 									}
-/******/ 								} else {
+/******/ 					// Load self accepted modules
+/******/ 					for (var o = 0; o < outdatedSelfAcceptedModules.length; o++) {
+/******/ 						var item = outdatedSelfAcceptedModules[o];
+/******/ 						var moduleId = item.module;
+/******/ 						try {
+/******/ 							item.require(moduleId);
+/******/ 						} catch (err) {
+/******/ 							if (typeof item.errorHandler === "function") {
+/******/ 								try {
+/******/ 									item.errorHandler(err, {
+/******/ 										moduleId: moduleId,
+/******/ 										module: __webpack_require__.c[moduleId]
+/******/ 									});
+/******/ 								} catch (err2) {
 /******/ 									if (options.onErrored) {
 /******/ 										options.onErrored({
-/******/ 											type: "self-accept-errored",
+/******/ 											type: "self-accept-error-handler-errored",
 /******/ 											moduleId: moduleId,
-/******/ 											error: err
+/******/ 											error: err2,
+/******/ 											originalError: err
 /******/ 										});
 /******/ 									}
 /******/ 									if (!options.ignoreErrored) {
+/******/ 										reportError(err2);
 /******/ 										reportError(err);
 /******/ 									}
 /******/ 								}
+/******/ 							} else {
+/******/ 								if (options.onErrored) {
+/******/ 									options.onErrored({
+/******/ 										type: "self-accept-errored",
+/******/ 										moduleId: moduleId,
+/******/ 										error: err
+/******/ 									});
+/******/ 								}
+/******/ 								if (!options.ignoreErrored) {
+/******/ 									reportError(err);
+/******/ 								}
 /******/ 							}
 /******/ 						}
-/******/ 					};
+/******/ 					}
 /******/ 		
-/******/ 					return Promise.all(acceptPromises)
-/******/ 						.then(onAccepted)
-/******/ 						.then(function () {
-/******/ 							return outdatedModules;
-/******/ 						});
+/******/ 					return outdatedModules;
 /******/ 				}
 /******/ 			};
 /******/ 		}
